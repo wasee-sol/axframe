1b8d5bbdf54e98da2500fb33b0bf8bec9e7f83d5
tom <tom@axisj.com>
2022-08-05 23:25:44 +0900

    wip Nav

--- src/components/nav/NavHeader.tsx
+++ src/components/nav/NavHeader.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import styled from "@emotion/styled";
+import { SMixinFlexRow } from "../../styles/emotion";
+import { ReactFrameLogo } from "react-frame-icon";
 
 interface Props {}
 
 function NavHeader(props: Props) {
-  return <NavHeaderContainer>NavHeader</NavHeaderContainer>;
+  return (
+    <NavHeaderContainer>
+      <Logo>
+        <ReactFrameLogo />
+        React Frame
+      </Logo>
+      <Handle>==</Handle>
+    </NavHeaderContainer>
+  );
 }
 
-const NavHeaderContainer = styled.div``;
+const NavHeaderContainer = styled.div`
+  ${SMixinFlexRow("stretch", "center")};
+`;
+
+const Logo = styled.div`
+  ${SMixinFlexRow("stretch", "center")};
+`;
+const Handle = styled.div``;
 
 export default NavHeader;
