86c95c74eb542db5dab5a92ae5d19cc665546d7e
tom <tom@axisj.com>
2022-11-01 17:22:06 +0900

    Fix errorDialog

--- src/components/dialogs/errorDialog.tsx
+++ src/components/dialogs/errorDialog.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-
 import { CloseCircleOutlined } from "@ant-design/icons";
 import { Modal } from "antd";
-import { useI18n } from "hooks/useI18n";
+import i18n from "i18n";
+import { useAppStore } from "stores";
 
 export interface IErrorDialogOptions {
   icon?: React.ReactNode;
@@ -23,19 +23,22 @@ const errorDialog = (options: IErrorDialogOptions, isMounted?: React.MutableRefO
       return resolve(true);
     }
 
-    const { t } = useI18n();
+    const currentLanguage = useAppStore.getState().currentLanguage;
+    const t = i18n[currentLanguage ?? "en"];
 
     Modal.error({
       icon: options.icon === null ? null : options.icon || <CloseCircleOutlined />,
       autoFocusButton: "ok",
-      // zIndex: Enums.ModalZIndex.ALERT_DIALOG,
       className: options.className,
       okText: t.button.ok,
       cancelText: t.button.cancel,
-      transitionName: "slidedown",
+      transitionName: "slide-down",
       title: options.title === null ? null : options.title || "Error",
       content: options.content || options.message || "Unknown error occurred",
       width: options.width,
+      bodyStyle: {
+        padding: 15,
+      },
       onOk() {
         resolve(true);
       },
