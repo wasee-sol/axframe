0099304c591eaeec9b793ab4f13b940eaacd2607
tom <tom@axisj.com>
2023-01-13 20:40:55 +0900

    migrate

--- src/i18n/pages/example.ts
+++ src/i18n/pages/example.ts
@@ -1,6 +1,18 @@
 export const EN_example = {
+  detail: {
+    title: "Detail Example",
+  },
+  form: {
+    title: "Form Example",
+  },
   list: {
     title: "List Example",
+  },
+  listAndDrawer: {
+    title: "List & Drawer",
+  },
+  listAndModal: {
+    title: "List & Modal",
   },
   listWithList: {
     title: "List & List",
@@ -10,8 +22,20 @@ export const EN_example = {
   },
 };
 export const KO_example: typeof EN_example = {
+  detail: {
+    title: "Detail 예제",
+  },
+  form: {
+    title: "폼 예제",
+  },
   list: {
     title: "목록 예제",
+  },
+  listAndDrawer: {
+    title: "목록 & 서랍",
+  },
+  listAndModal: {
+    title: "목록 & 모달",
   },
   listWithList: {
     title: "목록 & 목록",
