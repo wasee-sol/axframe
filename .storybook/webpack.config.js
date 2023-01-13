efa73067d7ed2f6bda4f2df3e9618f4b582c4f67
tom <tom@axisj.com>
2023-01-13 11:38:56 +0900

    Fix storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,19 +3,17 @@ const path = require("path");
 module.exports = ({ config }) => {
   config.resolve.alias = {
     ...config.resolve.alias,
-    "@hooks": path.resolve(__dirname, "../src/@hooks"),
-    "@template": path.resolve(__dirname, "../src/@template"),
+    "@core": path.resolve(__dirname, "../src/@core"),
     "@types": path.resolve(__dirname, "../src/@types"),
     components: path.resolve(__dirname, "../src/components"),
     hooks: path.resolve(__dirname, "../src/hooks"),
     i18n: path.resolve(__dirname, "../src/i18n"),
-    repository: path.resolve(__dirname, "../src/repository"),
+    pages: path.resolve(__dirname, "../src/pages"),
     router: path.resolve(__dirname, "../src/router"),
     services: path.resolve(__dirname, "../src/services"),
     stores: path.resolve(__dirname, "../src/stores"),
     stories: path.resolve(__dirname, "../src/stories"),
     styles: path.resolve(__dirname, "../src/styles"),
-    utils: path.resolve(__dirname, "../src/utils"),
   };
 
   return config;
