9a784141924fee16e6d1f7b20306acc68ef9c9e2
tom <tom@axisj.com>
2023-01-31 10:31:47 +0900

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -90,6 +90,8 @@ var config = {
             patterns: [
                 { from: "*.css", context: path.resolve(__dirname, "public") },
                 { from: "*.jpg", context: path.resolve(__dirname, "public") },
+                { from: "*.png", context: path.resolve(__dirname, "public") },
+                { from: "*.ico", context: path.resolve(__dirname, "public") },
                 { from: "404.html", context: path.resolve(__dirname, "public") },
             ]
         }),
